496bbf4ed310036fa05f9c372f167221c03c60c9
Henning Jacobs <henning@jacobs1.de>
2011-07-25 18:28:38 +0200

    average graph data over time

--- lib/checks.py
+++ lib/checks.py
@@ -1,5 +1,20 @@
+import itertools
 import operator
 import re
+
+def average_by_time(data, window_secs=30):
+    data_grouped = []
+    for key, group in itertools.groupby(data, lambda x: int(x[0]/window_secs)):
+        sumts = 0
+        sumtup = [0, 0, 0, 0, 0]
+        l = 0 
+        for ts, tup in group:
+            sumts += ts
+            for i, v in enumerate(tup):
+                sumtup[i] += v
+            l += 1
+        data_grouped.append((sumts/l, tuple(map(lambda x: x/l, sumtup))))
+    return data_grouped
 
 class AbstractCheck(object):
     def get_matching_probes(self, pages, minutes=60):
@@ -24,7 +39,7 @@ class AbstractCheck(object):
         return sum([ u.time_total for u in self.get_matching_probes(pages) ]) / len(self.get_matching_probes(pages))
     def get_graph_data(self, pages):
         data = [ (u[1], u[4:9]) for u in self.get_matching_probes(pages, minutes=500) ]
-        return data
+        return average_by_time(data)
     def evaluate(self, pages, validation_results):
         status = 'OK'
         probes = self.get_matching_probes(pages)
@@ -55,3 +70,14 @@ class UrlCheck(AbstractCheck):
             self.func = bool
     def matches(self, page, probe):
         return self.func(self.url_pattern.search(probe.url))
+
+if __name__ == '__main__':
+
+    data = [
+        (1, (1, 2, 3, 4, 5)),
+        (10, (1, 2, 3, 8, 5)),
+        (61, (9, 10, 11, 12, 13)),
+        (123, (3, 3, 3, 3, 3))
+    ]
+    print average_by_time(data, window_secs=60)
+
